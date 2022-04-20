8dfdd4fd197322a449b1c0bc6a5de9afeac9cac9
Bernat <bernattorras@gmail.com>
2022-04-20 13:04:20 +0200

    Update vip-noindex.php

--- vip-noindex.php
+++ vip-noindex.php
@@ -7,17 +7,17 @@
  * License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
-if ( defined( 'VIP_GO_APP_ENVIRONMENT' ) && 'production' !== VIP_GO_APP_ENVIRONMENT ) {
-	$is_public = get_option( 'blog_public', '1' );
+if ( 'production' !== VIP_GO_APP_ENVIRONMENT || false !== strpos( $_SERVER['HTTP_HOST'] ?? '', '.go-vip.' ) ) {
+	// Discourage search engine indexing.
+	add_filter( 'option_blog_public', '__return_zero' );
 
-	if ( '1' === $is_public ) {
-		// Disable the 'Search engine visibility' option if the blog is public.
-		update_option( 'blog_public', '0' );
-	}
-
-	// Show an admin notification.
+	// Show an admin notice to warn that the site visibility has been turned off.
 	add_action( 'admin_notices', 'admin_indexable_notice' );
+
+	// Disable the 'blog_public' checkbox.
+	add_action( 'admin_footer', 'disable_blog_public_option' );
 }
+
 
 /**
  * Show an admin notice to inform that search engine visibility has been turned off.
@@ -29,5 +29,21 @@ function admin_indexable_notice() {
 	<div class="notice notice-warning is-dismissible">
 		<p><?php esc_html_e( 'Search engine visibility has been turned off to prevent indexing this non-production site.', 'vip-mu-noindex' ); ?></p>
 	</div>
+	<?php
+}
+
+/**
+ * Disables the 'blog_public' checkbox in Settings > Reading.
+ *
+ * @return void
+ */
+function disable_blog_public_option() {
+	if ( 'options-reading' !== get_current_screen()->id ) {
+		return;
+	}
+	?>
+	<script>
+	jQuery('#blog_public').attr("disabled", true);
+	</script>
 	<?php
 }
