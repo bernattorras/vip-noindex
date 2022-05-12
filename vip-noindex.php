5bd0df9dcf6d06316921fcf2e1ef44d4c5d62238
Bernat <bernattorras@gmail.com>
2022-05-12 15:25:57 +0200

    Added robots.txt tweak and the 'vip_index_noprod_domains' filter
    - Added the disallow_all_user_agents() function to disallow all agents using robots.txt
    - Added the 'vip_index_noprod_domains' filter to let developers specify if the non-production domains should be indexed by default (return true) or specify the non-production domains that they want to index (adding the domains inside an array).

--- vip-noindex.php
+++ vip-noindex.php
@@ -7,7 +7,18 @@
  * License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
-if ( 'production' !== VIP_GO_APP_ENVIRONMENT || false !== strpos( $_SERVER['HTTP_HOST'] ?? '', '.go-vip.' ) ) {
+// By default, the 'vip_index_noprod_domains' filter returns an empty array (to prevent indexing any non-production sites).
+$index_noprod_domains = apply_filters( 'vip_index_noprod_domains', '__return_empty_array' );
+
+// Don't "no-index" the site if the filter 'vip_index_noprod_domains' returns true or contains the current domain in a string.
+if ( true === $index_noprod_domains || in_array( $_SERVER['SERVER_NAME'], $index_noprod_domains, true ) ) {
+	return;
+}
+
+// Block the indexing of non-production sites.
+if ( 'production' !== VIP_GO_APP_ENVIRONMENT || false !== strpos( $_SERVER['HTTP_HOST'] ?? '', '.go-vip.' )
+) {
+
 	// Discourage search engine indexing.
 	add_filter( 'option_blog_public', '__return_zero' );
 
@@ -16,7 +27,21 @@ if ( 'production' !== VIP_GO_APP_ENVIRONMENT || false !== strpos( $_SERVER['HTTP
 
 	// Disable the 'blog_public' checkbox.
 	add_action( 'admin_footer', 'disable_blog_public_option' );
+
+	// Disalow site in robots.txt.
+	add_action( 'do_robotstxt', 'disallow_all_user_agents' );
 }
+
+/**
+ * Disallow all user-agents for the site in robots.txt
+ *
+ * @return void
+ */
+function disallow_all_user_agents() {
+	echo 'User-agent: *' . PHP_EOL;
+	echo 'Disallow: /' . PHP_EOL;
+}
+
 
 /**
  * Show an admin notice to inform that search engine visibility has been turned off.
